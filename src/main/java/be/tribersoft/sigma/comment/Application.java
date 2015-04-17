8eafd1eb7eb6e9c43f3efaa1690e2897fa950a3b
geert olaerts <geertolaerts@geerts-MacBook-Pro.local>
2015-04-17 21:06:21 +0200

    added war packaging for tomcat

--- src/main/java/be/tribersoft/sigma/comment/Application.java
+++ src/main/java/be/tribersoft/sigma/comment/Application.java
@@ -6,11 +6,13 @@ import org.glassfish.jersey.jackson.JacksonFeature;
 import org.glassfish.jersey.server.ResourceConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
 
 import be.tribersoft.sigma.comment.rest.internal.CommentController;
 
 @SpringBootApplication
-public class Application {
+public class Application extends SpringBootServletInitializer {
 
 	@Named
 	public static class JerseyConfig extends ResourceConfig {
@@ -24,4 +26,10 @@ public class Application {
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(Application.class);
+	}
+
 }
