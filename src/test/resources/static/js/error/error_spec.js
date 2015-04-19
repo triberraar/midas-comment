6ea10d8e93c675e515532b338208307dd8c1a29e
geert olaerts <geertolaerts@geerts-MacBook-Pro.local>
2015-04-19 13:33:17 +0200

    added js unit test test

---
+++ src/test/resources/static/js/error/error_spec.js
@@ -0,0 +1,9 @@
+describe('error', function() {
+	describe('ErrorService', function() {
+		describe('getError', function() {
+			it('should return the error', function() {
+				expect(true).toBe(true)
+			})
+		})
+	})
+})
